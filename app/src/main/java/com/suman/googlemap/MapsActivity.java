ce32b29e55554e9cb4b950218d6aad595b281dba
SsumanShahi <shaggysy201118@gmail.com>
2020-01-16 10:24:33 +0545

    current location code

--- app/src/main/java/com/suman/googlemap/MapsActivity.java
+++ app/src/main/java/com/suman/googlemap/MapsActivity.java
@@ -1,8 +1,17 @@
 package com.suman.googlemap;
 
-import androidx.fragment.app.FragmentActivity;
-
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -16,9 +25,14 @@ import com.suman.googlemap.model.LatitudeLongitude;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleMap.OnMyLocationClickListener,
+        GoogleMap.OnMyLocationButtonClickListener,
+        ActivityCompat.OnRequestPermissionsResultCallback {
 
     private GoogleMap mMap;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private boolean mPermissionDenied = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +41,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
     }
 
 
@@ -42,21 +58,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         mMap = googleMap;
 
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
+
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        enableLocation();
+        Log.d("TAG","this is ");
         List<LatitudeLongitude> longitudes = new ArrayList<>();
-        longitudes.add(new LatitudeLongitude(27.6623825,85.3034549,"Mero Ghar"));
-        longitudes.add(new LatitudeLongitude(27.6618263,85.3034658,"Medicity Hospital"));
-        longitudes.add(new LatitudeLongitude(27.6617875,85.3030792,"Big Mart"));
+        longitudes.add(new LatitudeLongitude(27.6623825, 85.3034549, "Mero Ghar"));
+        longitudes.add(new LatitudeLongitude(27.6618263, 85.3034658, "Medicity Hospital"));
+        longitudes.add(new LatitudeLongitude(27.6617875, 85.3030792, "Big Mart"));
 //        longitudes.add(new LatitudeLongitude(27.7052354,85.3294158,"Softwarica College"));
 //        longitudes.add(new LatitudeLongitude(27.70482,85.3293997,"Sabin ko Pasal"));
 
         CameraUpdate center, zoom;
-        for(int i=0; i<longitudes.size();i++)
-        {
+        for (int i = 0; i < longitudes.size(); i++) {
             center = CameraUpdateFactory.newLatLng(new LatLng(longitudes.get(i).getLat(),
                     longitudes.get(i).getLon()));
-            zoom =CameraUpdateFactory.zoomTo(16);
+            zoom = CameraUpdateFactory.zoomTo(16);
             mMap.addMarker(new MarkerOptions().position(new LatLng(longitudes.get(i).getLat(),
                     longitudes.get(i).getLon())).title(longitudes.get(i).getMarker()));
 
@@ -69,5 +93,72 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //        LatLng nakkhu = new LatLng(27.6623825,85.3034549);
 //        mMap.addMarker(new MarkerOptions().position(nakkhu).title("Mero Ghar"));
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(nakkhu));
+
+
+    }
+
+    private void enableLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+            Log.d("TAG","lovation");
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+            Log.d("TAG","else if");
+
+        }
+        else {
+            Log.d("TAG","else if");
+
+        }
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableLocation();
+        } else {
+            // Display the missing permission error dialog when the fragments resume.
+            mPermissionDenied = true;
+        }
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (mPermissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            mPermissionDenied = false;
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getSupportFragmentManager(), "dialog");
     }
 }
